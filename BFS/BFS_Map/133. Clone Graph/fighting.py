ffb162c59bdafa18cdf66edf799bfce643c64e6d
Leonard <101617499+LeonardCareer@users.noreply.github.com>
2023-06-08 15:34:00 +1000

    133. Clone Graph

---
+++ BFS/BFS_Map/133. Clone Graph/fighting.py
@@ -0,0 +1,20 @@
+
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        oldToNew = {}
+        def dfs(node):
+            if node in oldToNew:
+                return oldToNew[node]
+            
+            copyNode = Node(node.val)
+            oldToNew[node] = copyNode
+            for nei in node.neighbors:
+                copyNode.neighbors.append(dfs(nei))
+            return copyNode
+        return dfs(node) if node else None
